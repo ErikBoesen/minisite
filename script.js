af8913fd3e647f3040c5fbdf09676f1adb382d90
Erik Boesen <boesene@c02mrgp6g083.home>
2015-09-02 11:50:01 -0400

    Added Google Analytics, moved all JS to independent file

---
+++ script.js
@@ -0,0 +1,13 @@
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-44442416-3', 'auto');
+ga('send', 'pageview');
+
+$(function(){
+    $("p").typed({
+        strings: ["Hey there.", "I'm Erik.", "I <a href='http://erikboesen.com/web'>make websites</a>.", "I take <a href='flickr.com/photos/108614122@N04'>photos</a>.", "I <a href='http://erikboesen.com/blog'>blog</a> things.", "More about me <a href='http://erikboesen.com'>here</a>."],
+        typeSpeed: 100
+    });
+});
