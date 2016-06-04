db5f1730985302e60a050ee6c0d74fc63d9b6f68
ErikBoesen <me@erikboesen.com>
2016-06-04 16:32:55 -0400

    Updated everything

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# minisite
+# Typing
 A hyper simplistic version of my website. Uses typed.js by Matt Boldt.
--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-if(document.location.protocol=='http:'){
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-    ga('create', 'UA-44442416-3', 'auto');
-    ga('send', 'pageview');
-}
-
 $(function(){
-    $("p").typed({
-        strings: ["Hey there.", "I'm Erik.", "I <a href='http://erikboesen.com/web'>make websites</a>.", "I take <a href='http://flickr.com/photos/108614122@N04'>photos</a>.", "I <a href='http://erikboesen.com/blog'>blog</a> things.", "More about me <a href='http://erikboesen.com'>here</a>."],
+    $('p').typed({
+        strings: ['Hey there.', 'I\'m Erik.', 'I <a href="https://chrome.google.com/webstore/detail/bwnt-new-tab/doiinciigjmmlnbehjjjkeoamihggkba">made a minimal New Tab Page</a>.', 'I built an <a href="https://frcdashboard.github.io">FRC robot driving interface library</a>.', 'Also a <a href="https://github.com/ErikBoesen/haiku">minimalistic WordPress theme</a>.', 'See my website <a href="http://erikboesen.com">here</a>.'],
         typeSpeed: 100
     });
 });
